cd93f7ca416b7d6e30c94e7aea61994e6b904cbe
Joy Evans <joyevans@me.com>
2020-11-21 23:24:11 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,6 @@
+# name = "Byron"
+name = "Luca"
+
+puts "we're sorry, but per health inspector's rules, #{name} is not alloed in the store."
+
+#=> We're soory, but per health inspector's rules, Luca is not allowed in the store.
